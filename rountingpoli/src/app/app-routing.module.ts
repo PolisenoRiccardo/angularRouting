f7bccfde0ebdc4f6a1d38c8b7ddc29200f5bea27
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-05-30 12:56:43 +0000

    butsrap

--- rountingpoli/src/app/app-routing.module.ts
+++ rountingpoli/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AnimalsComponent } from './animals/animals.component';
 import { FruitsComponent } from './fruits/fruits.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/animals', pathMatch: 'full'},
   { path: 'animals', component: AnimalsComponent},
   { path: 'fruits', component: FruitsComponent}
 ];
