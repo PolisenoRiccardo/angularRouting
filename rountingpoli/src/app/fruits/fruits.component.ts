93a6233f5eb81dc59f206ea9005dc692d8d86331
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-06-03 06:28:37 +0000

    frutta

--- rountingpoli/src/app/fruits/fruits.component.ts
+++ rountingpoli/src/app/fruits/fruits.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./fruits.component.css']
 })
 export class FruitsComponent {
+  fruits: Array<any>  = [
+    { name: 'teffa7', emoji: '🍎' },    // Mela
+    { name: 'limo', emoji: '🍋' },      // Limone
+    { name: 'bortoqal', emoji: '🍊' },  // Arancia
+    { name: 'muz', emoji: '🍌' },       // Banana
+    { name: '3inab', emoji: '🍇' },     // Uva
+    { name: 'fraoula', emoji: '🍓' },   // Fragola
+    { name: 'de7', emoji: '🍉' },       // Anguria
+    { name: 'frawla', emoji: '🍓' },    // Fragola
+    { name: 'kiwi', emoji: '🥝' },      // Kiwi
+    { name: 'khukh', emoji: '🍑' },     // Pesca
+    { name: 'le3nab', emoji: '🍇' },    // Uva
+    { name: 'bela', emoji: '🍐' },      // Pera
+    { name: '7emama', emoji: '🍈' },    // Melone
+    { name: 'ananass', emoji: '🍍' },   // Ananas
+    { name: 'mangha', emoji: '🥭' },    // Mango
+    { name: 'rumman', emoji: '🍏' },    // Melagrana
+    { name: '9roq', emoji: '🍒' },      // Ciliegia
+    { name: 'limoun', emoji: '🍋' },    // Limone
+    { name: 'teffah', emoji: '🍎' },    // Mela
+    { name: 'batalikh', emoji: '🍈' },  // Melone
+    { name: 'khukh', emoji: '🍑' },     // Pesca
+    { name: 'bwazzi', emoji: '🍌' },    // Banana
+    { name: 'laymon', emoji: '🍋' },    // Limone
+    { name: 'ananas', emoji: '🍍' },    // Ananas
+    { name: 'tufa7', emoji: '🍏' },     // Mela
+    { name: '3inab', emoji: '🍇' },     // Uva
+    { name: 'tin', emoji: '🍉' },       // Anguria
+    { name: 'zabib', emoji: '🍇' },     // Uva secca
+    { name: 'battikh', emoji: '🍈' },   // Melone
+    { name: 'shmam', emoji: '🍈' },     // Melone
+    { name: 'ballout', emoji: '🍈' },   // Melone
+    { name: 'bess', emoji: '🍋' },      // Limone
+    { name: 'lawzi', emoji: '🍑' },     // Pesca
+    { name: 'farawla', emoji: '🍓' },   // Fragola
+    { name: 'rman', emoji: '🍏' },      // Melagrana
+    { name: 'tanger', emoji: '🍊' },    // Mandarino
+    { name: 'bardq', emoji: '🍊' },     // Arancia
+    { name: 'lazwart', emoji: '🍇' }    // Uva blu
+];
 
 }
